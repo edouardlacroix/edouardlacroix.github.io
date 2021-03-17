0a5344c1e52411f87b63a0f026a44557afbdf003
Edouard LACROIX <edouard.lacroix.ext@groupealixia.fr>
2021-03-17 11:29:27 +0100

    Fix on topbar display

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -5,46 +5,43 @@ import TopBar from '../../components/TopBar'
 export default function Home() {
     return (
         <div className={styles.wrapper}>
-            <Head>
-                <TopBar />
-            </Head>
-            <div className={styles.horizontalBar}>
-                <h1>Technologies</h1>
-                <h2>JavaScript</h2>
+            <TopBar />
+            <div className={styles.content_wrapper}>
 
+                <div className={styles.horizontalBar}>
+                    <h1>Technologies</h1>
+                    <h2>JavaScript</h2>
+
+                </div>
+
+                <div className={styles.horizontalBar}>
+                    <h1>UnityLearning</h1>
+
+                </div>
+                <div className={styles.horizontalBar}>
+                    <h1>LoveLetterFront</h1>
+
+                </div>
+                <div className={styles.horizontalBar}>
+                    <h1>LoveLetterServer</h1>
+
+                </div>
+                <div className={styles.horizontalBar}>
+                    <h1>LovEat (React Native)</h1>
+
+                </div> <div className={styles.horizontalBar}>
+                    <h1>SoundBox</h1>
+
+                </div> <div className={styles.horizontalBar}>
+                    <h1>LovEat (PWA)</h1>
+
+                </div>
+
+                <div className={styles.horizontalBar}>
+                    <h1>LovEat (Strapi)</h1>
+
+                </div>
             </div>
-
-            <div className={styles.horizontalBar}>
-                <h1>UnityLearning</h1>
-
-            </div>
-            <div className={styles.horizontalBar}>
-                <h1>LoveLetterFront</h1>
-
-            </div>
-            <div className={styles.horizontalBar}>
-                <h1>LoveLetterServer</h1>
-
-            </div>
-            <div className={styles.horizontalBar}>
-                <h1>LovEat (React Native)</h1>
-
-            </div> <div className={styles.horizontalBar}>
-                <h1>SoundBox</h1>
-
-            </div> <div className={styles.horizontalBar}>
-                <h1>LovEat (PWA)</h1>
-
-            </div>
-
-            <div className={styles.horizontalBar}>
-                <h1>LovEat (Strapi)</h1>
-
-            </div>
-
-
-
-
         </div>
     )
 }
